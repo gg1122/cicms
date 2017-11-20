35195f548309484e1ace142888f449427fa89da4
uckendo <455019825@qq.com>
2017-11-20 15:33:54 +0800

    ignore文件忽略

--- application/views/sys/version/index.php
+++ application/views/sys/version/index.php
@@ -2,12 +2,11 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>layui</title>
+    <title><?=$title?></title>
     <meta name="renderer" content="webkit">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="<?= $this->config->item('base_url') ?>/assets/css/layui.css" media="all">
-    <!-- 注意：如果你直接复制所有代码到本地，上述css路径需要改成你本地的 -->
 </head>
 <body>
 
